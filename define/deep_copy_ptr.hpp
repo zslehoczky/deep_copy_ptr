a952d7bada6b47435ed984ebf0fa07c3f34bca70
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-10-02 18:08:06 +0200

    Add copy mechanism

--- define/deep_copy_ptr.hpp
+++ define/deep_copy_ptr.hpp
@@ -8,6 +8,35 @@ namespace final
     class deep_copy_ptr : public std::unique_ptr<T, D>
     {
     public:
-        explicit deep_copy_ptr(T *val = nullptr) : std::unique_ptr<T, D>{val} {}
+        explicit deep_copy_ptr(T *val = nullptr)
+            : std::unique_ptr<T, D>{val}
+        {
+        }
+
+        deep_copy_ptr(const deep_copy_ptr &other)
+            : std::unique_ptr<T, D>{other.create_deep_copy_of_ptr(), other.get_deleter()}
+        {
+        }
+
+        deep_copy_ptr &operator=(const deep_copy_ptr &other)
+        {
+            this->reset(other.create_deep_copy_of_ptr());
+
+            return *this;
+        }
+
+        deep_copy_ptr(deep_copy_ptr &&other) = default;
+        deep_copy_ptr &operator=(deep_copy_ptr &&other) = default;
+
+    private:
+        [[nodiscard]] T *create_deep_copy_of_ptr() const
+        {
+            if (this->operator bool())
+            {
+                return new T{this->operator*()};
+            }
+
+            return nullptr;
+        }
     };
 }
--- test/test_deep_copy_ptr.cpp
+++ test/test_deep_copy_ptr.cpp
@@ -6,5 +6,24 @@ using namespace final;
 
 int main()
 {
-    deep_copy_ptr<int> val(new int{1});
+    const deep_copy_ptr<int> val{new int{1}};
+
+    assert(val);
+    assert(*val == 1);
+
+    deep_copy_ptr<int> val2{val};
+
+    assert(val2);
+    assert(*val2 == 1);
+
+    assert(val.get() != val2.get());
+
+    deep_copy_ptr<int> val3{};
+
+    assert(!val3);
+
+    val3 = val;
+
+    assert(val3);
+    assert(*val3 == 1);
 }
